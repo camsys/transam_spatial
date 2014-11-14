362ebefad9e5333a2c58b2f1f97495f2d2c96c22
Julian Ray <jray@camsys.com>
2014-11-14 09:33:11 -0500

    Turn off validation for location references

--- lib/transam_spatial/transam_geo_locatable.rb
+++ lib/transam_spatial/transam_geo_locatable.rb
@@ -27,7 +27,7 @@ module TransamSpatial
       validates :location_reference,      :presence => true
   
       # custom validator for location_reference
-      validate  :validate_location_reference
+      #validate  :validate_location_reference
           
     end
   
@@ -87,7 +87,7 @@ module TransamSpatial
       
       # Fail validation if the location reference type is not set
       if self.location_reference_type_id.nil?
-        return false
+        return true
       end
       
       # If the user explicitly set the type to NULL then always pass validation
