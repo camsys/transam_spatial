7b6784dfc31f87fee6331e74825d22a99fd83762
Aaron Magil <amagil@camsys.com>
2014-12-03 15:25:52 -0500

    Create TransamGeocodable module for use when geocoded lat/long

--- lib/transam_spatial.rb
+++ lib/transam_spatial.rb
@@ -1,5 +1,6 @@
 require "transam_spatial/engine"
 require "transam_spatial/transam_geo_locatable"
+require "transam_spatial/transam_geocodable"
 
 module TransamSpatial
 end
---
+++ lib/transam_spatial/transam_geocodable.rb
@@ -0,0 +1,62 @@
+############################################################################### 
+# 
+# TransamSpatial::TransamGeocodable
+# Unlike geolocatable, which populates a geometry (WellKnownText?) attribute,
+# .geocodable populates lat/long fields based on an address.  This is simply
+# a point on the map, rather than an area.
+# 
+###############################################################################
+module TransamSpatial
+  module TransamGeocodable
+    extend ActiveSupport::Concern
+    
+    included do
+  
+      # ----------------------------------------------------  
+      # Callbacks
+      # ----------------------------------------------------  
+      
+      # Enable automatic geocoding using the Geocoder gem
+      geocoded_by       :full_address
+      after_validation  :geocode  
+      
+      # ----------------------------------------------------  
+      # Associations
+      # ----------------------------------------------------  
+         
+      # ----------------------------------------------------  
+      # Validations
+      # ----------------------------------------------------  
+      
+          
+    end
+  
+    #------------------------------------------------------------------------------
+    #
+    # Class Methods
+    #
+    #------------------------------------------------------------------------------
+  
+    module ClassMethods
+  
+    end
+  
+    #------------------------------------------------------------------------------
+    #
+    # Instance Methods
+    #
+    #------------------------------------------------------------------------------
+    
+    # This method defines required fields for any model which is geocodable
+    def full_address
+      elems = []
+      elems << address1 unless address1.blank?
+      elems << address2 unless address2.blank?
+      elems << city unless city.blank?
+      elems << state unless state.blank?
+      elems << zip unless zip.blank?
+      elems.compact.join(', ')    
+    end
+    
+  end      
+end
