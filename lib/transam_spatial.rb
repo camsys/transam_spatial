0e5100d7610e91eb849c6fee3c21c7bc13338c7b
Aaron Magil <amagil@camsys.com>
2014-12-08 10:33:15 -0500

    Removing reference to transam_spatial_controller.  That's not built yet.

--- lib/transam_spatial.rb
+++ lib/transam_spatial.rb
@@ -2,7 +2,6 @@ require "transam_spatial/engine"
 require "transam_spatial/transam_geo_locatable"
 require "transam_spatial/transam_address_locatable"
 require "transam_spatial/transam_geocodable"
-require "transam_spatial/transam_spatial_controller"
 
 module TransamSpatial
 
