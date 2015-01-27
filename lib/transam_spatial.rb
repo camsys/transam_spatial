18a92a149364e7af4002350fd15c54f34bc019ac
Julian Ray <jray@camsys.com>
2015-01-27 10:18:52 -0500

    Add geoJSON processing

--- lib/transam_spatial.rb
+++ lib/transam_spatial.rb
@@ -1,5 +1,6 @@
 require "transam_spatial/engine"
 require "transam_spatial/transam_geo_locatable"
+require "transam_spatial/transam_geo_JSON_feature"
 require "transam_spatial/transam_address_locatable"
 require "transam_spatial/transam_geocodable"
 
---
+++ lib/transam_spatial/transam_geo_JSON_feature.rb
@@ -0,0 +1,74 @@
+#-------------------------------------------------------------------------------
+#
+# GeoJSONFeature
+#
+# Injects methods for managing a class as a RGeo feature collection
+#
+# Usage:
+#   Add the following line to an asset class
+#
+#   Include TransamGeoJSONFeature
+#
+#_------------------------------------------------------------------------------
+module TransamGeoJSONFeature
+  extend ActiveSupport::Concern
+
+  included do
+
+    # ----------------------------------------------------
+    # Callbacks
+    # ----------------------------------------------------
+
+    # ----------------------------------------------------
+    # Associations
+    # ----------------------------------------------------
+
+    # ----------------------------------------------------
+    # Validations
+    # ----------------------------------------------------
+
+    # ----------------------------------------------------
+    # Attributes
+    # ----------------------------------------------------
+    class_attribute :geometry_attribute_name
+    class_attribute :property_names
+
+  end
+
+  #-----------------------------------------------------------------------------
+  #
+  # Class Methods
+  #
+  #-----------------------------------------------------------------------------
+
+  module ClassMethods
+
+    def acts_as_mappable(options = {})
+      self.geometry_attribute_name = (options[:geometry_attribute_name] || "geometry").to_s
+      self.property_names = (options[:property_names] || ["id"])
+    end
+
+  end
+
+  #-----------------------------------------------------------------------------
+  #
+  # Instance Methods
+  #
+  #-----------------------------------------------------------------------------
+  def to_geoJSON
+    g = self.send(geometry_attribute_name)
+    props = {}
+    props[:id] = self.object_key
+    property_names.each {|x| props[x] = self.send(x).to_s}
+    unless g.nil?
+      {
+        type: 'Feature',
+        geometry: {
+          type: 'Point',
+          coordinates: [g.longitude, g.latitude]
+        },
+        properties: props
+      }
+    end
+  end
+end
