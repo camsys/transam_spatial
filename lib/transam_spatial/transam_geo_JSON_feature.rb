4b85258c192305c144cf7c7d5ac472a969c67cac
Julian Ray <jray@camsys.com>
2015-02-04 08:41:21 -0500

    PRovide callback for obtaining a feature-specific set of properties for the geoJSON feature

--- lib/transam_spatial/transam_geo_JSON_feature.rb
+++ lib/transam_spatial/transam_geo_JSON_feature.rb
@@ -9,6 +9,16 @@
 #
 #   Include TransamGeoJSONFeature
 #
+# Each implementing class will need to provide the following instance method
+# def geoJSON_property_names
+#   [:attr1, :attr2]
+# end
+#
+# where attr1 and attr2 are properties and have a valid to_s implementation.
+# This method should return a set of attributes that will be loaded into the
+# geoJSON feature. Note that :id => :object_key is always added and does not
+# need to be included in the list
+#
 #_------------------------------------------------------------------------------
 module TransamGeoJSONFeature
   extend ActiveSupport::Concern
@@ -31,7 +41,6 @@ module TransamGeoJSONFeature
     # Attributes
     # ----------------------------------------------------
     class_attribute :geometry_attribute_name
-    class_attribute :property_names
 
   end
 
@@ -45,7 +54,6 @@ module TransamGeoJSONFeature
 
     def configure_geojson(options = {})
       self.geometry_attribute_name = (options[:geometry_attribute_name] || "geometry").to_s
-      self.property_names = (options[:property_names] || ["id"])
     end
 
   end
@@ -55,11 +63,12 @@ module TransamGeoJSONFeature
   # Instance Methods
   #
   #-----------------------------------------------------------------------------
+
   def to_geoJSON
     g = self.send(geometry_attribute_name)
     props = {}
     props[:id] = self.object_key
-    property_names.each {|x| props[x] = self.send(x).to_s}
+    geoJSON_property_names.each {|x| props[x] = self.send(x).to_s}
     unless g.nil?
       {
         type: 'Feature',
