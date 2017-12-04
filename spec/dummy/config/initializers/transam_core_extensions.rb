b04ea415ec72adb577def828c4af33b24de9db3e
mathmerized <mathmerized@gmail.com>
2017-12-04 16:01:22 -0500

    set factory for dummy app

--- spec/dummy/config/initializers/transam_core_extensions.rb
+++ spec/dummy/config/initializers/transam_core_extensions.rb
@@ -2,3 +2,5 @@
 # Defines services to use
 Rails.application.config.new_user_service = "NewUserService"
 Rails.application.config.policy_analyzer = "PolicyAnalyzer"
+
+Rails.application.config.rgeo_factory = RGeo::Geographic.spherical_factory(srid: 4326)
