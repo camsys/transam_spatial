b6d6c18167c87d4c9d6f0ae395f339e97488ae5c
mathmerized <mathmerized@gmail.com>
2018-06-15 08:30:14 -0400

    add rgeo config at app not engine

--- config/initializers/transam_spatial.rb
+++ config/initializers/transam_spatial.rb
@@ -1,11 +1,1 @@
 Rails.application.config.max_num_map_objects = 1000
-
-if defined?(RGeo)
-  RGeo::ActiveRecord::SpatialFactoryStore.instance.tap do |config|
-    # By default, use the GEOS implementation for spatial columns.
-    config.default = RGeo::Geos.factory_generator
-
-    # But use a geographic implementation for point columns.
-    config.register(Rails.application.config.rgeo_factory, geo_type: "geometry")
-  end
-end
