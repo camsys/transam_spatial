135c9aaed46d4ce46932ecb8287697dcf3df21bc
Alex Andrade <aandrade@camsys.com>
2018-11-28 10:11:23 -0500

    Revert previous changes to geocoding test.

--- spec/models/transam_geodocable_spec.rb
+++ spec/models/transam_geodocable_spec.rb
@@ -6,7 +6,7 @@ RSpec.describe TransamGeocodable do
     let(:test_org) { create(:organization, address1: "123 Fake Street", city: "Bloston", state: "MA", zip: "00000") }
 
     it 'is located in Massachusetts' do
-      expect(test_org.latitude).to be_within(0.000001).of(42.360083)
+      expect(test_org.latitude).to be_within(0.000001).of(42.407211)
       expect(test_org.longitude).to be_within(0.000001).of(-71.382437)
     end
 
