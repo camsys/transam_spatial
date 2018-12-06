4ff2e1e53134a038d5bb5594b3cf7c573649d60a
Alex Andrade <aandrade@camsys.com>
2018-12-05 19:07:22 -0500

    Delete unnecessary factories.

--- spec/factories/fta_asset_categories.rb
+++
@@ -1,8 +0,0 @@
-FactoryBot.define do
-
-  factory :fta_transit_assets_category, :class => :fta_asset_category do
-    name { 'Transit Assets' }
-    active { true }
-  end
-
-end
--- spec/factories/manufacturer_models.rb
+++
@@ -1,7 +0,0 @@
-FactoryBot.define do
-  factory :manufacturer_model do
-    name { 'Transam Model' }
-    description { "A generalized manufacturer model." }
-    active { false }
-  end
-end
