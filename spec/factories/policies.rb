7f4cdf7b86931c07ea514a0a45dd9fa43e8d8a05
Xudong Liu <xudong.camsys@gmail.com>
2018-12-10 11:17:48 -0500

    convert to dynamic attribtues

--- spec/factories/policies.rb
+++ spec/factories/policies.rb
@@ -7,11 +7,11 @@ FactoryBot.define do
     cost_calculation_type_id { 1 }
     condition_estimation_type_id { 1 }
     condition_threshold { 2.5 }
-    name 'TestPolicy'
-    description 'Test Policy'
-    year Date.today.year
-    current true
-    active true
+    name { 'TestPolicy' }
+    description { 'Test Policy' }
+    year { Date.today.year }
+    current { true }
+    active { true }
   end
 
   factory :policy do
