848c589adff411e22e343c5cf40765257a3e105f
Alex Andrade <aandrade@camsys.com>
2019-02-21 14:53:05 -0500

    Try changing syntax to fix travis test.

--- spec/factories/map_overlay_services.rb
+++ spec/factories/map_overlay_services.rb
@@ -1,8 +1,8 @@
 FactoryBot.define do
   factory :map_overlay_service do
     name { "Test Name" }
-    organization
-    map_overlay_service_type
+    association :organization
+    association :map_overlay_service_type
     sequence :url do |n|
       "http://#{n}"
     end
