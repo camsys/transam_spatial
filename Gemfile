5fe80bff420665b69bdac8c07a1b243b296fd03c
mathmerized <mathmerized@gmail.com>
2020-06-24 13:00:03 -0400

    updated schema, Gemfile, and fix geometry view

--- Gemfile
+++ Gemfile
@@ -9,10 +9,10 @@ gem 'active_record-acts_as', git: 'https://github.com/camsys/active_record-acts_
 
 # To use debugger
 # gem 'debugger'
-gem 'transam_core', git: 'https://github.com/camsys/transam_core', branch: :master
+gem 'transam_core', git: 'https://github.com/camsys/transam_core', branch: 'quarter2'
 
 # needed for testing spatial mixins on transit models
-gem 'transam_transit', git: 'https://github.com/camsys/transam_transit', branch: :master
+gem 'transam_transit', git: 'https://github.com/camsys/transam_transit', branch: 'quarter2'
 
 gem 'mysql2', "~> 0.5.1" # lock gem for dummy app
 gem "capybara", '2.6.2' # lock gem for old capybara behavior on hidden element xpath
--- db/seeds.rb
+++ db/seeds.rb
@@ -71,19 +71,11 @@ merge_tables.each do |table_name|
 end
 
 puts "======= Loading spatial asset query configurations ======="
-if ActiveRecord::Base.configurations[Rails.env]['adapter'].include? 'mysql2'
-  view_sql = <<-SQL
-          CREATE OR REPLACE VIEW geometry_transam_assets_view AS
-          SELECT transam_assets.id, X(geometry) as longitude, Y(geometry) as latitude
-          FROM transam_assets
-  SQL
-elsif ActiveRecord::Base.configurations[Rails.env]['adapter'].include? 'post'
-  view_sql = <<-SQL
+view_sql = <<-SQL
           CREATE OR REPLACE VIEW geometry_transam_assets_view AS
           SELECT transam_assets.id, ST_X(geometry) as longitude, ST_Y(geometry) as latitude
           FROM transam_assets
-  SQL
-end
+SQL
 
 ActiveRecord::Base.connection.execute view_sql
 
